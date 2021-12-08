5815a6f1aa69b9b3196d4b6c51ae5df1178d74f5
Yontida Yosbut <a.yontida@gmail.com>
2021-12-08 16:17:37 -0500

    added pics

--- src/components/AllDogsList.js
+++ src/components/AllDogsList.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function AllDogsList() {
+function AllDogsList(props) {
   return (
     <div >
+      <h2>All DOGS</h2>
 
-      
+      <br></br>
 
+      {/* {props.dogs.map( (eachDogObj) => {
+
+
+        return eachDogObj.image
+        
+      }
+      )} */}
 
     </div>
   );
--- src/components/DogMainContainer.js
+++ src/components/DogMainContainer.js
@@ -1,21 +1,60 @@
 import React, { useEffect, useState } from "react";
+import AllDogsList from "./AllDogsList";
 
 
 function DogMainContainer() {
 
   const [ dogs, setDogs ] = useState( [] )
+  console.log("STATE of dogs: ", dogs)
 
   const baseURL = "http://localhost:3000/dogs"
 
+
 useEffect( () => {
 
-  
+  fetch(baseURL)
+  .then( r => r.json() )
+  .then( 
+    ( fetchedData ) => {
+        setDogs( fetchedData ) 
+    } 
+  )
 }, [])
+
+
+// function randeringAllDogs () {
+//   dogs.map( 
+    
+  //   (eachDogObj) => {
+
+  //   return eachDogObj.name
+  // } )
+
+// }
 
 
   return (
     <div >
 
+      <h2>Hey - testing DogMainContainer</h2>
+      <br></br>
+
+      {/* <AllDogsList 
+          dogs={dogs}
+      /> */}
+
+        {dogs.map( (eachDogObj) => {
+
+
+        return (
+          <>
+        <h5>{eachDogObj.name}</h5>
+        <img src={eachDogObj.image} />
+        </>
+        )
+
+        }
+        )}
 
 
     </div>
